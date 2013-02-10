c313dcb537be8ad79af4466afab2cf2776c1ee9b
Sam Van der Borght <samvanderborght@gmail.com>
2013-02-10 13:41:17 +0100

    lua

--- includes/battery.lua
+++ includes/battery.lua
@@ -7,7 +7,30 @@ baticon:set_image(beautiful.widget_batt)
 
 batwidget = wibox.widget.textbox()
 
-vicious.register( batwidget, vicious.widgets.bat, "<span color=\"#8faf5f\">$1$2% [$3]</span>", 30, "BAT0")
+vicious.register( batwidget, vicious.widgets.bat, 
+	function(widget, args)
+
+		local current = args[2]
+		if current < 10 and args[1] == "-" then
+			  color = beautiful.fg_widget_value_important
+			  -- Maybe we want to display a small warning?
+			  if current ~= batwidget.lastwarn then
+			     batwidget.lastid = naughty.notify(
+				{ title = "Battery low!",
+				  preset = naughty.config.presets.critical,
+				  timeout = 20,
+				  text = "Battery level is currently " ..
+				     current .. "%.\n" .. args[3] ..
+				     " left before running out of power.",
+				  beautiful.widget_batt_empty,
+				  replaces_id = batwidget.lastid }).id
+			     batwidget.lastwarn = current
+			  end
+       	end     
+		return "<span color=\"#8faf5f\">" .. args[1] .. args[2] .. "% [" .. args[3] .. "]</span>"
+	end, 30, "BAT0")
+
+
 
 
 -- }}
