d3754f6045dd45f23c199024d43496736877affc
Sam Van der Borght <samvanderborght@gmail.com>
2013-02-10 01:32:42 +0100

    added extra lua

--- includes/battery.lua
+++ includes/battery.lua
@@ -1,11 +1,17 @@
 -- {{ Battery 
 
+-- Requires Saidar Unix utility 
+--
+-- #pacman -S libstatgrab pystatgrab
+-- 
+-- When battery is clicked this curses util will be spawned
+
 baticon = wibox.widget.imagebox()
 baticon:set_image(beautiful.widget_batt)
 
 batwidget = wibox.widget.textbox()
 
-vicious.register( batwidget, vicious.widgets.bat, "<span color=\"#8faf5f\">$1 $2 $3%</span>", 2, "BAT0")
+vicious.register( batwidget, vicious.widgets.bat, "<span color=\"#8faf5f\">[$1] $2% [$3]</span>", 2, "BAT0")
 baticon:buttons(awful.util.table.join(
     awful.button({ }, 1, function () awful.util.spawn("".. terminal.. " -geometry 80x18 -e saidar -c", false) end)
 ))
--- rc.lua
+++ rc.lua
@@ -76,7 +76,7 @@ beautiful.init(active_theme .. "/theme.lua")
 
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "xterm"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
