a074024383d0b1cd958a045591a94b84cc9857ee
Sam Van der Borght <samvanderborght@gmail.com>
2013-02-10 01:33:55 +0100

    F

--- includes/battery.lua
+++ includes/battery.lua
@@ -12,7 +12,11 @@ baticon:set_image(beautiful.widget_batt)
 batwidget = wibox.widget.textbox()
 
 vicious.register( batwidget, vicious.widgets.bat, "<span color=\"#8faf5f\">[$1] $2% [$3]</span>", 2, "BAT0")
+
 baticon:buttons(awful.util.table.join(
+    awful.button({ }, 1, function () awful.util.spawn("".. terminal.. " -geometry 80x18 -e saidar -c", false) end)
+))
+batwidget:buttons(awful.util.table.join(
     awful.button({ }, 1, function () awful.util.spawn("".. terminal.. " -geometry 80x18 -e saidar -c", false) end)
 ))
 
