827255865796cc9a7b0bc3eb23c3f1fb5676acba
Sam Van der Borght <samvanderborght@gmail.com>
2013-02-15 16:12:42 +0100

    fix rc.lua

--- rc.lua
+++ rc.lua
@@ -258,6 +258,7 @@ for s = 1, screen.count() do
     left_layout:add(space)
     left_layout:add(mypromptbox[s])
     left_layout:add(space)
+    left_layout:add(mylayoutbox[s])
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
